b9511f54e9adb35cc467a27b3df08b0bb2bcb669
EPG-UmairBhatti <umair.bhatti@easypay-group.com>
2025-06-27 09:55:13 +0200

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NewCustomer} from './new-customer/new-customer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewCustomer],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-angular-app';
+
+
 }
